41101b2b9ab83157bfb7b7af9a83b83dffa0148b
Perumal Palani <perumalp@uit.ac.in>
2025-03-21 10:49:39 +0530

    order changed

--- src/App.js
+++ src/App.js
@@ -11,13 +11,18 @@ function App() {
     <div className="ps-3">
       <h1>Video Player with Quality Auto Selection</h1>
       <div className="display:flex">
+
+
+        <p>HLS (HTTP Live Streaming) supports resolutions from 4K to 240p and is compatible with all devices.</p>
+        <HLSPlayer src="https://pub-04bd08a612a343d3bdf972452bfee388.r2.dev/hls_with_audio/master.m3u8" width="410px" height="240px" />
+
+        <hr className="my-4" />
+
         <p>DASH (Dynamic Adaptive Streaming over HTTP) supports resolutions from 4K to 240p and is supported on all Android devices. However, it is not supported on Apple devices.</p>
         <DashPlayer src="https://pub-04bd08a612a343d3bdf972452bfee388.r2.dev/dash/output.mpd" width="410px" height="240px" />
       </div>
 
-      <hr className="my-4"/>
-      <p>HLS (HTTP Live Streaming) supports resolutions from 4K to 240p and is compatible with all devices.</p>
-      <HLSPlayer src="https://pub-04bd08a612a343d3bdf972452bfee388.r2.dev/hls_with_audio/master.m3u8" width="410px" height="240px" />
+
     </div>
   );
 }
